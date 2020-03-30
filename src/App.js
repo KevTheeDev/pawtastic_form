fc8d988bbe010f6087721fd22c883798a1b9c083
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-30 00:27:31 -0500

    added .bind methods and label in form

--- src/App.js
+++ src/App.js
@@ -10,15 +10,19 @@ class Pawtastic extends React.Component {
     // add empty text because we are expecting text
     this.state = { value: " :name" }
 
-
-
+    // binding handles changes that are happening
+    // your onClick/onChange events won’t work since they won’t have any context as to what “this” is and so it will call this.undefine.methodName() and fail
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   render() { 
     return (     
     <div className="App">
       <header className="App-header">
-        <form>
-
+        <form onSubmit = {this.handleSubmit}>
+          <label>
+          </label>
         </form>
 
         {/* <img src={logo}className="App-logo" alt="logo"/> */}
