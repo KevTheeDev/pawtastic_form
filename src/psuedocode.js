c44d7d97df67bc95c9e11ce6d7c3a7a05883e948
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-31 00:59:22 -0500

    added references

---
+++ src/psuedocode.js
@@ -0,0 +1,13 @@
+/*
+
+                            REFERENCES
+
+For REACT form names, ages, etc..
+https://www.youtube.com/watch?v=qH4pJISKeoI
+
+Tried to figure out columns
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_two_columns_flex
+
+
+
+*/
